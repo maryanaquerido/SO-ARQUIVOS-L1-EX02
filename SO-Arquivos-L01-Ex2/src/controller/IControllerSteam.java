aba2f7a9e7c7fb6e9d92bc01908dfde330fbd35e
Maryana <maryana.carmo@fatec.sp.gov.br>
2024-11-06 17:22:35 -0300

    Update IControllerSteam.java

--- SO-Arquivos-L01-Ex2/src/controller/IControllerSteam.java
+++ SO-Arquivos-L01-Ex2/src/controller/IControllerSteam.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public interface IControllerSteam {
 	
 	public void consultaUsuario(String year, String month, String avg, String path, String name) throws IOException;
-	
 	public void createFile(String arqName,String path, String arquivoCriado, String year, String month) throws IOException;
-	;
+	
 }
